72909d1b7c62c7e1100d0c35e94ab33431f2db31
hamizmz <root@graham-robertson.ca>
2016-06-20 21:28:34 -0400

    🎉  Initial commitgit add .

---
+++ application/src/main.ts
@@ -0,0 +1,6 @@
+var log: any = require('terminal-kit').terminal;
+
+log.white('\n****************************************\n');
+log.white("* ").yellow("code").white('\n');
+log.white("* ").red('Generated: ').white((new Date()).toString()).white("\n");
+log.white('****************************************\n\n');
